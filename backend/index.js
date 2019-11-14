8d1c68ccea0aeead7de8ca0d82eda6779414165d
Žilinskis, Andrejs <andrejs.zilinskis@seb.se>
2019-11-14 14:18:09 +0200

    Revert "Revert "Setting up the webpack for the deployment""
    This reverts commit 21f3e92322c68dbf2aed5554d23510ea3dfdfd58.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 var cors = require('cors')
-const app = require('express')()
+const express = require('express');
+const app = express();
 const { ApolloServer } = require('apollo-server-express')
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers') 
 const bookingAPI = require('./graphql/datasources/cars')
+const path = require('path');
+const bodyParser = require('body-parser');
+const router = require('./routes/routes.js');
 
 const server = new ApolloServer({ 
   typeDefs,
@@ -16,9 +20,11 @@ const server = new ApolloServer({
 
 
 server.applyMiddleware({ app })
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../frontend'));
+app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+app.use('/', router);
 
-const PORT = 4000
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-})
+module.exports = app;
