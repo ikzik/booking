8178ce9fc9659d9e1683b8a2e125d8524d2b4649
Žilinskis, Andrejs <andrejs.zilinskis@seb.se>
2019-11-14 17:50:28 +0200

    fixing the host issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,7 @@ const cache = new InMemoryCache()
 const client = new ApolloClient({
   cache,
   link: new HttpLink({
-    uri: process.env.REACT_APP_HOST + '/graphql'
+    uri: '/graphql'
   })
 })
 
